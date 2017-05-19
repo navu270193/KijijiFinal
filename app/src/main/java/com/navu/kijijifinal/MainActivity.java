92c78de9534f6b768f438c93618d5bced4b88fa2
Navu <rainavi10@gmail.com>
2017-05-19 12:10:54 -0400

    i had done my all 3 screens

--- app/src/main/java/com/navu/kijijifinal/MainActivity.java
+++ app/src/main/java/com/navu/kijijifinal/MainActivity.java
@@ -1,13 +1,56 @@
 package com.navu.kijijifinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView im = (ImageView) findViewById(R.id.imageView12);
+        Button bt = (Button) findViewById(R.id.button);
+
+
+
+
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), selectCategory.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+    }
+
+    void mainClick(View view) {
+        setContentView(R.layout.activity_main2);
+
+    }
+    void mainClick(Button view) {
+        setContentView(R.layout.activity_select_category);
+
     }
 }
