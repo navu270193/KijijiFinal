fef2826c631ed464a4d3e8e7398d162d86cf64be
Navu <rainavi10@gmail.com>
2017-05-19 12:18:50 -0400

    put links for home icon on favourivote page

--- app/src/main/java/com/navu/kijijifinal/Main2Activity.java
+++ app/src/main/java/com/navu/kijijifinal/Main2Activity.java
@@ -1,7 +1,11 @@
 package com.navu.kijijifinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ImageView im = (ImageView) findViewById(R.id.imageView9);
+        //Button bt = (Button) findViewById(R.id.button);
+
+
+
+
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+    }
+    void mainClick(View view) {
+        setContentView(R.layout.activity_main);
+
     }
 }
