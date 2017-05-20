906e04229e45b218adf882d09eb9496b59f9c289
Navu <rainavi10@gmail.com>
2017-05-20 13:02:58 -0400

    Post Screen

---
+++ app/src/main/java/com/navu/kijijifinal/MyKijiji.java
@@ -0,0 +1,13 @@
+package com.navu.kijijifinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyKijiji extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_kijiji);
+    }
+}
--- app/src/main/java/com/navu/kijijifinal/Post.java
+++ app/src/main/java/com/navu/kijijifinal/Post.java
@@ -13,8 +13,20 @@ public class Post extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
         ImageView imag = (ImageView) findViewById(R.id.imageView9);
+        ImageView ima1 = (ImageView) findViewById(R.id.imageView13);
+        ImageView imab = (ImageView) findViewById(R.id.imageView10);
 
 
+        imab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyKijiji.class);
+                startActivity(intent);
+            }
+
+
+
+        });
         imag.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,10 +35,26 @@ public class Post extends AppCompatActivity {
             }
 
 
+
+        });
+        ima1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyMessages.class);
+                startActivity(intent);
+            }
         });
     }
     void mainClick(View view) {
         setContentView(R.layout.activity_main);
+
+    }
+    void mainClick1(View view) {
+        setContentView(R.layout.activity_my_messages);
+
+    }
+    void mainClick2(View view) {
+        setContentView(R.layout.activity_my_kijiji);
 
     }
 }
