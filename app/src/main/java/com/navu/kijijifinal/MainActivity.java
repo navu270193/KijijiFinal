d6126e2d1679a107c7e9869fdea70f2ae93f7427
1691509 <parminderpal23@gmail.com>
2017-05-20 13:50:00 -0400

    provide link to faviourate

--- app/src/main/java/com/navu/kijijifinal/MainActivity.java
+++ app/src/main/java/com/navu/kijijifinal/MainActivity.java
@@ -21,10 +21,19 @@ public class MainActivity extends AppCompatActivity {
         Button bt = (Button) findViewById(R.id.button);
         ImageView ima = (ImageView) findViewById(R.id.imageView11);
         ImageView imag1 = (ImageView) findViewById(R.id.imageView10);
-
+        ImageView imag2 = (ImageView) findViewById(R.id.imageView12);
 
 
         im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+
+
+        });
+        imag2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
@@ -77,6 +86,10 @@ public class MainActivity extends AppCompatActivity {
     }
     void mainClick3(View view) {
         setContentView(R.layout.activity_my_kijiji);
+
+    }
+    void mainClick4(View view) {
+        setContentView(R.layout.activity_main2);
 
     }
 }
