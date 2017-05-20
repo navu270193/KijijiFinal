94595fa7231f44cfa843b08dd1bff14b573add8a
Navu <rainavi10@gmail.com>
2017-05-20 13:41:12 -0400

    put link on screen home on image My kijiji

--- app/src/main/java/com/navu/kijijifinal/MyKijiji.java
+++ app/src/main/java/com/navu/kijijifinal/MyKijiji.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MyKijiji extends AppCompatActivity {
-
+//navu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
